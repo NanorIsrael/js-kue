82ec8622e99a436ba45e02bd889b115d97faa1a1
nanorisrael <israel.nanor@compozelabs.com>
2024-03-05 22:28:27 +0000

    Add kue job

--- job_creator.js
+++ job_creator.js
@@ -12,25 +12,7 @@ const job = queue.create('push_notification_code', jobData)
     if (!err) {
       console.log('Notification job created:', job.id);
     }
-  });
-
-// Process the job
-queue.process('push_notification_code', (job, done) => {
-  // Simulate a failure
-  // done(new Error('Failed to send notification'));
-
-  // Simulate a successful job
-  console.log('Sending notification:', job.data);
-  done();
 });
 
-// Event handlers for completed and failed jobs
-queue.on('job complete', (id) => {
-  console.log('Notification job completed');
-});
-
-queue.on('job failed', (id, err) => {
-  console.log('Notification job failed');
-});
 
 module.exports = queue;
---
+++ job_processor.js
@@ -0,0 +1,47 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+
+// Process the job
+queue.process('push_notification_code', (job, done) => {
+	// Simulate a failure
+	// done(new Error('Failed to send notification'));
+	sendNotification(job.data['phoneNumber'], job.data['message'])
+	done();
+});
+
+
+// Event handlers for completed and failed jobs
+queue.on('job complete', (id) => {
+	console.log('Notification job completed');
+});
+
+queue.on('job failed', (id, err) => {
+	console.log('Notification job failed');
+});
+
+// Listen to the queue
+queue.on('ready', () => {
+	queue.process('push_notification_code', (job, done) => {
+	  console.log('Processing job:', job.id);
+	  // Simulate a successful job
+	  done();
+	});
+});
+  
+queue.on('error', (err) => {
+	console.log('Queue error:', err);
+});
+
+
+
+function sendNotification(phoneNumber, message) {
+	// Event handlers for completed and failed jobs
+	queue.on('job complete', () => {
+		console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+	});
+
+	queue.on('job failed', (id, err) => {
+		console.log('Notification job failed');
+	});
+}
